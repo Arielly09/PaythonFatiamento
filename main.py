0406fe8fa205d0091b1dc55c97f9d4ca81e2996b
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-08-21 12:11:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 url = "bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
-print(url)
 
-url_base = url[0:19]
-print(url_base)
+indice_fatiamento = url.find('?')
+url_base = url[0:indice_fatiamento]
+
+indice = url.find('&')
+url_parametros = url[indice_fatiamento+1:indice]
+print(url_parametros)
+
+parametro_busca= 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+
+print(indice_fatiamento)
